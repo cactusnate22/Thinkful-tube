63d52b9d7927db091e39edc059a8a61ab23b7fc3
Nathan Ash <nash311@gmail.com>
2017-09-22 17:24:09 -0400

    added API KEY as a string

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const youtubeSearch_URL = "https://www.googleapis.com/youtube/v3/search";
 function getDataFromApi(searchTerm, callback) {
 	const query = {
 		part:'snippet',
-		key: AIzaSyC9xc2W_AWe_0k54KE6PYIR4JDQMH5QY1s,
+		key: "AIzaSyC9xc2W_AWe_0k54KE6PYIR4JDQMH5QY1s",
 		q: `${searchTerm}`,
 		per_page: 5
 	}
 	$.getJSON(youtubeSearch_URL, query, callback);
+	//need to create variables for searchTerm and callback?
+	//const searchTerm = $(`.js-query`);
 }
 
 // function displayResults//
@@ -25,6 +27,8 @@ function renderResults(result) {
 function displayYouTubeSearchData(data) {
 	const results = data.items.map((item, index)=> renderResult(item));
 	$('.js-search-results').html(results);
+	//console.log(displayYouTubeSearchData(data));
+	alert('Hi Im working');
 }
 
 function watchSubmit() {
