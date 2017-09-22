92e3b5ba40098ec6b08882a5b010ff5ff3f35c9c
Nathan Ash <nash311@gmail.com>
2017-09-22 10:44:46 -0400

    added js

---
+++ index.js
@@ -0,0 +1,38 @@
+const youtubeSearch = "AIzaSyC9xc2W_AWe_0k54KE6PYIR4JDQMH5QY1s";
+
+function getDataFromApi(searchTerm, callback) {
+	const query = {
+		q: `${searchTerm} in:name`,
+		per_page: 5
+	}
+}
+
+// function displayResults//
+function renderResults(result) {
+	return `
+	<div>
+	   <h2>
+	     <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a>
+	   </h2>
+	</div>
+  `;
+}
+
+//function to display search data returned
+function displayYouTubeSearchData(data) {
+	const results = data.items.map((item, index)=> renderResult(item));
+	$('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event=> {
+  	event.preventDefault();
+  	const queryTarget = $(event.currentTarget).find('.js-query');
+  	const query = queryTarget.val();
+  	//this clears the input
+  	queryTarget.val("");
+  	getDataFromApi(query, displayYouTubeSearchData);
+  });
+}
+
+$(watchSubmit);
